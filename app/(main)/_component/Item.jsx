f60908c507ba814c96e77e77275818813f4ff31d
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-04-12 19:26:26 -0700

    added the plus button and its functionality

--- app/(main)/_component/Item.jsx
+++ app/(main)/_component/Item.jsx
@@ -1,11 +1,41 @@
 "use client"
 
-import {ChevronDown, ChevronRight, LucideIcon} from 'lucide-react'
+import {ChevronDown, ChevronRight, LucideIcon, Plus} from 'lucide-react'
 import { cn } from '@/lib/utils'
+import { toast } from 'sonner'
 import { Skeleton } from '@/components/ui/skeleton'
+import { useMutation } from 'convex/react'
+import { api } from '@/convex/_generated/api'
+import { useRouter } from 'next/navigation'
 
 const Item = ({id,label, onClick, icon: Icon, active, documentIcon, isSearch, level=0, onExpand, expanded}) => {
- 
+  
+  const router = useRouter()
+  const create = useMutation(api.documents.create)
+  const handleExpand = (event) => {
+    event.stopPropagation()
+    onExpand?.();
+  }
+
+  const onCreate = (event) => {
+    event.stopPropagation()
+    if(!id) return;
+    const promise = create({
+      title: "Untitled",
+      parentDocument : id
+    }).then((documentId) => {
+      if(!expanded){
+        onExpand?.()
+      }
+      // router.push(`/documents/${documentId}`)
+    })
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note creates!",
+      error: "Failed to create a new note"
+    })
+  }
   const ChevronIcon = expanded ? ChevronDown : ChevronRight
 
   
@@ -14,7 +44,7 @@ const Item = ({id,label, onClick, icon: Icon, active, documentIcon, isSearch, le
     onClick={onClick}
     role="button"
     style={{
-      paddingLeft: level ? `${(level*12) + 12}` : "12px"
+      paddingLeft: level ? `${(level*12) + 12}px` : "12px"
     }}
     className={cn(
       "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
@@ -23,7 +53,7 @@ const Item = ({id,label, onClick, icon: Icon, active, documentIcon, isSearch, le
         {!!id && (
           <div role='button'
           className='h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1'
-          onClick={() => {}}>
+          onClick={handleExpand}>
             <ChevronIcon
              className='h-4 w-4 shrink-0 text-muted-foreground/50'/>
           </div>
@@ -45,6 +75,13 @@ const Item = ({id,label, onClick, icon: Icon, active, documentIcon, isSearch, le
             CMD
           </span>
         </kbd>
+      )}
+      {!!id && (
+        <div role='button' onClick={onCreate} className='ml-auto flex items-center gap-x-2'>
+          <div className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600'>
+            <Plus className='h-4 w-4 text-muted-foreground'/>
+          </div>
+        </div>
       )}
     </div>
   )
